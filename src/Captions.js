f80efd6b372143f6237035873cdd73878158c38c
Mike Steele <mike@steel.fm>
2019-02-05 21:06:00 -0500

    Implement extra-space and captionStyle

--- src/Captions.js
+++ src/Captions.js
@@ -22,8 +22,28 @@ class Captions extends React.Component {
       return null;
     }
 
+    const captionProps = {};
+    if (adapter.captionStyle) {
+      captionProps.style = {...adapter.captionStyle};
+    }
+    if (settings.extraSpace) {
+      captionProps.className = 'extra-space';
+    }
+
+    // Replace \n's with <br/> elements
+    const captionToRender = currentCaptionToRender.split('\n').map(sentence => (
+      <React.Fragment>
+        <span>{sentence}</span>
+        <br/>
+      </React.Fragment>
+    ));
+
     if (captionWindow) {
-      return ReactDOM.createPortal((<div>{currentCaptionToRender}</div>), captionWindow);
+      return ReactDOM.createPortal((
+        <div {...captionProps}>
+          {captionToRender}
+        </div>
+      ), captionWindow);
     } else {
       return null;
     }
